7a767f385c8213636f1e4c4345e67d21042859d0
ahwlsqja <100296444+ahwlsqja@users.noreply.github.com>
2025-07-02 13:04:00 +0900

    백준 11651: 좌표 정렬하기 2 풀이

---
+++ week5/좌표_정렬하기_2.py
@@ -0,0 +1,10 @@
+n = int(input())
+points = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+points.sort(key=lambda x: (x[1], x[0]))
+
+for x, y in points:
+    print(x, y)
